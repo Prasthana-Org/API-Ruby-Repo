9f54d793a8970b0738e30d12a6f01b71d39ce868
sanjay-prasthana <sanjay.dora@prasthana.com>
2022-11-08 12:06:21 +0530

    This is an sample application commit test

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,49 @@
+class ArticlesController < ApplicationController
+
+  def about
+    render plain: "About Articles"
+  end
+
+  def index
+    @articles=Article.all
+    render json: @article
+  end
+
+  def create
+   @article =Article.new(article_params)
+   if @article.save
+     render json: @article, status: :ok
+   else
+    render json: {error: "#{article} is not created"}
+    render 'new'
+   end
+  end
+
+  def new
+    @article=Article.new()
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    render json: @article
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    @article.save
+    render json: @article
+  end
+
+    def destroy
+      @article=Article.find(params[:id])
+      @article.destroy
+    end
+
+    private
+
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  root 'articles#about'
+  get 'articles',to: 'articles#index'
+  resources :articles
+end
