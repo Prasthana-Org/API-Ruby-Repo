dbe3a3d1c4ec31db10d1f156df7efdf149984679
prasthana <argulashiva529@gmail.com>
2022-11-14 18:28:56 +0530

    first commit

--- app/controllers/employee_resources_controller.rb
+++ app/controllers/employee_resources_controller.rb
@@ -1,38 +1,28 @@
-# class EmployeeResourcesController < ApplicationController
-
-#     def index
-#     @employees_resources = EmployeeResource.all
-#     render json:@employees_resources,except: [ :created_at, :updated_at]
-#   end
-
-#   def create
-#     @employee_resource = EmployeeResource.new(employee_resource_params)
-#     @employee_resource.save
-#     render json:@employee_resource
-#   end
-
-#   def show
-#     @employee_resource= EmployeeResource.find(params[:id])
-#     render { json:@employee_resource,root: false}
-#   end
+class EmployeeResourcesController < ApplicationController
 
 
-#   def update
-#     @employee_resource =EmployeeResource.find(params[:id])
-#     @employee_resource.update(employee_resource_params)
-#     render json: @employee_resource
-#   end
+  def create
+    @employee_resource = EmployeeResource.new(employee_resource_params)
+    @employee_resource.save
+    render json:@employee_resource
+  end
 
-#   def destroy
-#     @employee_resource =EmployeeResource.find(params[:id])
-#     @employee_resource.destroy
-#     render json:@employee_resource
-#   end
+  def update
+    @employee_resource =EmployeeResource.find(params[:id])
+    @employee_resource.update(employee_resource_params)
+    render json: @employee_resource
+  end
 
-#   private
+  def destroy
+    @employee_resource =EmployeeResource.find(params[:id])
+    @employee_resource.destroy
+    render json:@employee_resource
+  end
 
-#   def employee_resource_params
-#     params.require(:employee_resource).permit(:resource_id, :employee_id, :Quantity, :date_of_issue)
-#   end
+  private
+
+  def employee_resource_params
+    params.require(:employee_resource).permit(:resource_id, :employee_id, :Quantity, :date_of_issue)
+  end
   
-# end
+end
--- app/serializers/employee_resource_serializer.rb
+++ app/serializers/employee_resource_serializer.rb
@@ -1,3 +1,7 @@
 class EmployeeResourceSerializer < ActiveModel::Serializer
-  attributes :id, :Quantity, :employee_id, :resource_id
+  attributes :id, :Quantity, :employee_id, :resource_id, :url
+
+  def url
+    employee_url(object)
+  end
 end
